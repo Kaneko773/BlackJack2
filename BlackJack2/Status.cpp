4a994343ea1ada0b1433417559fedf5b6942e2f6
Kaneko773 <harukakaneko773@gmail.com>
2023-11-07 10:36:52 +0900

    奇跡を加味して持てる枚数を増やした

--- BlackJack2/Status.cpp
+++ BlackJack2/Status.cpp
@@ -6,8 +6,8 @@ Card MakeRandomCard();
 
 //コンストラクタ
 Status::Status() {
-    _pcard = new Card[11];
-    for (int i = 0; i < 11; ++i) {
+    _pcard = new Card[12];
+    for (int i = 0; i < 12; ++i) {
         _pcard[i] = MakeRandomCard();
     }
     _outwardlyNum = 1;
@@ -15,8 +15,8 @@ Status::Status() {
 }
 //コピーコンストラクタ
 Status::Status(const Status& other) {
-    _pcard = new Card[11];
-    for (int i = 0; i < 11; ++i) {
+    _pcard = new Card[12];
+    for (int i = 0; i < 12; ++i) {
         _pcard[i] = other._pcard[i];
     }
     _outwardlyNum = other._outwardlyNum;
